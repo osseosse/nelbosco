b30070bac524534a9f98deb89ebbcc7eb77a0309
hypark023 <hypark023@192.168.26.42>
2023-06-28 11:11:04 +0900

    넬보스코 루프탑 템플릿 전면 수정

--- Nelbosco/src/main/java/com/nelbosco/service/MailService.java
+++ Nelbosco/src/main/java/com/nelbosco/service/MailService.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
 import com.nelbosco.domain.MailDTO;
 
 @Service
 @AllArgsConstructor
 public class MailService {
     private JavaMailSender mailSender;
-    private static final String FROM_ADDRESS = "kid3326@gmail.com";
+    private static final String FROM_ADDRESS = "넬보스코체크 <hypark023@osse.co.kr>";
 
     public void mailSend(MailDTO mailDto) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -21,4 +22,19 @@ public class MailService {
 
         mailSender.send(message);
     }
+
+	public void sendMail(String recAddress, String subject, String content) {
+		try {
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setTo(recAddress);
+			message.setFrom(MailService.FROM_ADDRESS);
+			message.setSubject(subject);
+			message.setText(content);
+
+			mailSender.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
